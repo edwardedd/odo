eec632d31cd8a17f867cb8ae73b0bdbf5dd5520e
edwardedd <eddmos86@gmail.com>
2019-09-17 18:51:18 +0300

    add product popup

---
+++ js/index.js
@@ -0,0 +1,21 @@
+
+
+$(".btn").on("click", function() {
+
+  const $button = $(this);
+  var oldValue = $button.parent().find("input").val();
+
+  if ($button.text() == "+") {
+	  var newVal = parseFloat(oldValue) + 1;
+	} else {
+   
+    if (oldValue > 0) {
+      var newVal = parseFloat(oldValue) - 1;
+    } else {
+      newVal = 0;
+    }
+  }
+
+  $button.parent().find("input").val(newVal);
+
+});
